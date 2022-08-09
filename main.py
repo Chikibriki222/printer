6fb07ce5cb351b8f3fe424caa6e6eb43b0435762
sekeresh.d.i.1.19 <sekeresh.d.i.1.19@gmail.com>
2022-08-09 14:20:15 +0300

    теперь печатает по 4 файла

--- main.py
+++ main.py
@@ -1,33 +1,46 @@
 from tkinter import *
 import os
 import time
+from tkinter import scrolledtext
 
 path = './files'
 i_files = os.listdir(path)
 
 
 def clicked():
+    counter = 0
     for file in i_files:
         file_path = f'{path}\\{file}'
 
         if os.path.isfile(file_path):
             try:
-                os.startfile(file_path, 'print')
-                print(f'Печатается {file}')
+                while counter <= 4:
+                    os.startfile(file_path, 'print')
+                    txt.insert(INSERT, f'Печатается {file}\n')
+                    time.sleep(5)
+                    counter = counter+1
+                counter = 0
 
-                time.sleep(5)
+
+
             except:
-                print(f'Внимание: {file} не удалось распечатать!')
+                txt.insert(INSERT, f'Внимание: {file} не удалось распечатать!\n')
+
 
 
 window = Tk()
 window.title('Принтер двухсторонних документов')
-window.geometry('400x250')
+window.geometry('800x600')
 
-lbl = Label(window, text='Нажми на кнопку для печати')
-lbl.grid(column=4, row=0)
+lbl = Label(window, text='Нажми на кнопку для печати', font=("monospace", 20),pady=10)
+lbl.grid(column=0, row=0)
 
-btn = Button(window, text='Печатать', command=clicked)
-btn.grid(column=4, row=5)
+btn = Button(window, text='Печатать', command=clicked, fg="black",font=("monospace", 14), width=72, pady=20)
+btn.grid(column=0, row=1)
+
+txt = scrolledtext.ScrolledText(window, width=100, height=20)
+txt.grid(column=0, row=2)
+
+
 
 window.mainloop()
